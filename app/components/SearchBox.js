218cba957031715561c6b2c2bbef2cf1b54a6bf3
Vedran Mandić <mandic.vedran@gmail.com>
2016-09-08 23:31:29 +0200

    fetched Weather API data to console

--- app/components/SearchBox.js
+++ app/components/SearchBox.js
@@ -1,12 +1,51 @@
 // development imports:
 var React = require("react");
+var PropTypes = React.PropTypes;
+var WeatherApi = require("../helpers/WeatherApi.js");
+var axios = require("axios");
 
 var SearchBox = React.createClass({
+    propTypes: {
+        className: PropTypes.string
+    },
+
+    getInitialState: function () {
+        return {
+            city: ""
+        };
+    },
+    handleSearchOnClick: function (e) {
+        e.preventDefault();
+        console.log(this.state.city);
+        axios.all(
+        [
+            WeatherApi.getCurrentWeather(this.state.city),
+            WeatherApi.getDayForecast(this.state.city)
+        ]).then(function (data) {
+            console.log("DATA: ", data);
+        }).catch(function(err){
+            console.error(err);
+        });
+    },
+    handleCityOnChange: function (e) {
+        this.setState({ city: e.target.value });
+    },
+
     render: function () {
         return (
             <div className={this.props.className}>
-                <input type="text" placeholder="Zagreb" className="form-control" />
-                <button style={{ margin: 10 + "px" }} className="btn btn-success">Get Weather</button>
+                <input
+                    type="text"
+                    placeholder="Zagreb"
+                    className="form-control"
+                    onChange={this.handleCityOnChange} />
+                <button
+                    style={{ margin: 10 + "px" }}
+                    className="btn btn-success"
+                    type="button"
+                    onClick={this.handleSearchOnClick}>
+                    Get Weather
+                </button>
             </div>
         );
     }
---
+++ app/helpers/WeatherApi.js
@@ -0,0 +1,15 @@
+var axios = require("axios");
+
+var API_KEY_WAPI = "b714ec74bbab5650795063cb0fdf5fbe";
+
+var WeatherApi = {
+    getCurrentWeather: function (cityName) {
+        return axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&type=accurate&APPID=" + API_KEY_WAPI);
+    },
+
+    getDayForecast: function (cityName) {
+        return axios.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&type=accurate&APPID=" + API_KEY_WAPI + "&cnt=5");
+    }
+};
+
+module.exports = WeatherApi;
